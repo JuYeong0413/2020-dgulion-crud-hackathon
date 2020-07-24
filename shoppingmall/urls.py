d848b29f5f4757993fe9c2702fbb87327821853a
Juyeong Lee <juyeonglee0413@gmail.com>
2020-07-25 02:41:21 +0900

    base, navbar, main

--- shoppingmall/urls.py
+++ shoppingmall/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main, name="main"),
     path('products/', include('products.urls')),
     path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shoppingmall/views.py
+++ shoppingmall/views.py
@@ -1,1 +1,4 @@
 from django.shortcuts import render
+
+def main(request):
+    return render(request, 'main.html')
