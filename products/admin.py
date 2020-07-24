9dfbd2c18381fdf5167b1f4695b71f13a664abb3
Juyeong Lee <juyeonglee0413@gmail.com>
2020-07-25 02:48:36 +0900

    product model

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'price',
+        'stock',
+        'seller',
+        'created_at',
+    )
+    search_fields = (
+        'name',
+    )
--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    image = models.ImageField(upload_to='images/', null=True)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
