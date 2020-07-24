ceddcab9d2aa4120663f3158535368bcd87c4fea
Juyeong Lee <juyeonglee0413@gmail.com>
2020-07-25 02:58:59 +0900

    products urls

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,9 @@ from .views import *
 
 app_name="products"
 urlpatterns = [
-    
+    path('', main, name="main"),
+    path('new/', create, name="create"),
+    path('<int:post_id>/', show, name="show"),
+    path('<int:post_id>/edit/', update, name="update"),
+    path('<int:post_id>/delete/', delete, name="delete"),
 ]
