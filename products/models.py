a338082568e326be43b65a34d985b60aa38686e6
Juyeong Lee <juyeonglee0413@gmail.com>
2020-08-04 00:48:44 +0900

    Add review model

--- products/models.py
+++ products/models.py
@@ -11,3 +11,12 @@ class Product(models.Model):
     seller = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    rating = models.IntegerField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
