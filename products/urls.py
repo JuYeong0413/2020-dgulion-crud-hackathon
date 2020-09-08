87cad64f7c4aa4b7e1a4aa0f39ff11ed09a458c8
Juyeong Lee <2jy22@naver.com>
2020-09-09 03:03:18 +0900

    좋아요 url

--- products/urls.py
+++ products/urls.py
@@ -27,6 +27,9 @@ urlpatterns = [
     path('<int:review_id>/delete_review', delete_review, name="delete_review"),
     # url에 리뷰 id/delete_review가 붙은 경로의 이름(name)은 delete_review, products의 views에 있는 delete_review 함수를 실행한다.
     # review_id의 값으로 리뷰 id값을 가지고 넘어간다.
+    path('<int:product_id>/like', like_product, name="like_product"),
+    # url에 상품 id/like가 붙은 경로의 이름(name)은 like_product, products의 views에 있는 like_product 함수를 실행한다.
+    # product_id의 값으로 상품 id값을 가지고 넘어간다.
 ]
 # Django 템플릿 태그(template tag)로 url을 작성할 때 {% url '앱이름:path이름' %} 의 형태로 작성한다.
 # {% url 'products:show' product.id %}
